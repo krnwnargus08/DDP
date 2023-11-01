81723e6e290dfefc6ace70c23184ba9ba3ac942a
krnwnargus08 <149573663+krnwnargus08@users.noreply.github.com>
2023-11-01 16:05:00 +0700

    Add files via upload

---
+++ DDP.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+// __ __ __
+//|__|__|__|
+//|__|__|__|
+//|__|__|__|	
+
+
+for(int a=3;a>=1;a--){
+	int b=1;
+	for(int b=1;b<=7;b++){
+	
+	if(b%2==0){
+
+		cout<<"___";
+	}else{
+		cout<<"|";
+	}
+		
+	}
+	cout<<endl;
+
+}
+	
+}
